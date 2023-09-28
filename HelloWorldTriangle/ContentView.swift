5a7d2b625ff0ac584e2e4f648130e954227cf0b8
Hexeption <minecraftfun201@gmail.com>
2023-09-28 20:53:25 +0100

    Initial Commit

---
+++ HelloWorldTriangle/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HelloWorldTriangle
+//
+//  Created by Keir Davis on 28/09/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HelloWorldTriangle/HelloWorldTriangleApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldTriangleApp.swift
+//  HelloWorldTriangle
+//
+//  Created by Keir Davis on 28/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldTriangleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
